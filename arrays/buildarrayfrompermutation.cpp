6d123fed01be691a531adb090e9a97704c5bbb87
leoxP <patrabaibhab1@gmail.com>
2023-09-08 20:28:13 +0530

    .

---
+++ arrays/buildarrayfrompermutation.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        vector<int> v;
+        for(int i=0;i<nums.size();i++){
+            v.push_back(nums[nums[i]]);
+        }
+        return v;
+    }
+};
