fb53c2a27c36aca82e57c3b117278a9aa11743f1
leoxP <patrabaibhab1@gmail.com>
2023-09-09 14:57:48 +0530

    .

---
+++ arrays/stringmatchinginanarray.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> stringMatching(vector<string>& words) {
+        vector<string> v;
+        for(int i=0;i<words.size();i++){
+            for(int j=0;j<words.size();j++){
+                if(i!=j && words[j].find(words[i])!=string::npos){
+                        v.push_back(words[i]);
+                        break;
+                }
+            }
+        }
+        return v;
+    }
+};
