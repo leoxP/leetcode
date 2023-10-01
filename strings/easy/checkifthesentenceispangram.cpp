38e789fcf011c2f0604fe2b63e12f2b39baf87c3
leoxP <patrabaibhab@gmail.com>
2023-10-01 18:04:49 +0530

    .

---
+++ strings/easy/checkifthesentenceispangram.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+       set<char> s;
+       for(auto x:sentence)
+           s.insert(x);
+        return s.size()==26;
+    }
+};
