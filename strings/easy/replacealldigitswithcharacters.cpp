1147782a2e0e0ae138a053055fead94a523dcc4d
leoxP <patrabaibhab@gmail.com>
2023-10-03 19:39:27 +0530

    .

---
+++ strings/easy/replacealldigitswithcharacters.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string replaceDigits(string s) {
+        for(int i=1;i<s.size();i+=2){
+            s[i]=char(s[i-1]+s[i]-'0');
+        }
+        return s;
+    }
+};
