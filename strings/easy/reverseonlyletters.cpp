b8c23796cda53f80c5cb34a04d4687076f66e086
leoxP <patrabaibhab@gmail.com>
2023-10-03 21:07:56 +0530

    .

---
+++ strings/easy/reverseonlyletters.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int l=0,r=s.size()-1;
+        while(l<r){
+            if(!isalpha(s[l])){
+                l++;
+                continue;
+            }
+            if(!isalpha(s[r])){
+                r--;
+                continue;
+            }
+            swap(s[l],s[r]);
+            l++;
+            r--;
+        }
+        return s;
+    }
+};
