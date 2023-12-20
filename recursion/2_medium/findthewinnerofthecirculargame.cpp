817247a3ad8c9c93071eb6e590dd83b399324f11
leoxP <patrabaibhab@gmail.com>
2023-12-20 11:57:50 +0530

    .

---
+++ recursion/2_medium/findthewinnerofthecirculargame.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int solver(vector<int> v,int s,int k,int n){
+    if(v.size()==1) return v[0];
+    s=(s+k-1)%v.size();
+    v.erase(v.begin()+s);
+    return solver(v,s,k,n);
+}
+
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        vector<int> v;
+        for(int i=0;i<n;i++){
+            v.push_back(i+1);
+        }
+        int s=0;
+        return solver(v,s,k,n);
+    }
+};
